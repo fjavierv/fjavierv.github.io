f2354f80bde96c5df22783ba15261506e203786d
Francisco Javier Vazquez Pua <geko.pua@gmail.com>
2014-03-09 11:06:28 +0100

    Centralización de configuración y inclusión de google analytics

---
+++ js/config.js
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+// Configuracion
+////////////////////////////////////////////////////////////////////////////////
+var config = {
+
+    sections: [{ id: "home",    desc: "Intro" },
+               { id: "photo",   desc: "Fotografía" },
+               { id: "article", desc: "Artículos" },
+               { id: "about",   desc: "Sobre mí" },
+               { id: "contact", desc: "Contacto" }],
+
+    restAPI: "https://api.flickr.com/services/rest/",
+    api_key: "f97265778564609a85d45b9444b21cd4",
+    user_id: "110747985@N04",
+
+    // Id del div donde añadiremos Galleria.io
+    containerID: "galleria", 
+
+    // Tamaño de las imagenes en la galería
+    sizePhoto: "big", //(thumb, small, medium, big, original)
+
+    // Tiempo en aparecer/desaparecer la galleria
+    galleriaDisappearTime: 1000,
+
+    // Texto boton mostrar galería
+    textShowGallery: "Visitar galería",
+
+    // Tag para poner música en los albunes de Flickr
+    tagMusic: "music:",
+}
